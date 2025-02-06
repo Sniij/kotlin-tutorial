b2f944364f8b8f2e3de07676f772ae79e435c27e
Sniij <120099321+Sniij@users.noreply.github.com>
2025-02-06 22:09:07 +0900

    add: Kotlin String

---
+++ src/main/kotlin/var/KotlinString.kt
@@ -0,0 +1,27 @@
+package main.kotlin.`var`
+
+import java.util.*
+
+class KotlinString {
+
+    fun accessString(){
+        var txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        println(txt[0]) // first element (A)
+        println(txt[2]) // third element (C)
+        println("The length of the txt string is: " + txt.length)
+        println(txt.lowercase(Locale.getDefault()))   // Outputs "abcdeghi..."
+
+        var txt1 = "Hello World"
+        var txt2 = "Hello World"
+        println(txt1.compareTo(txt2))  // Outputs 0 (they are equal)
+
+        var txtIndex = "Please locate where 'locate' occurs!"
+        println(txtIndex.indexOf("locate"))  // Outputs 7
+
+
+        var firstName = "John"
+        var lastName = "Doe"
+        println("My name is $firstName $lastName")
+    }
+
+}
